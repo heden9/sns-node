d5b8ed522fca5b2d420a9233c27c9c563b2e070d
mangx <771854332@qq.com>
2017-10-07 13:43:16 +0800

    1.0.0 beta

---
+++ model/api.js
@@ -0,0 +1,12 @@
+var db = require('./db');
+
+exports.sendByNamePwd = function(data,callback){
+    var sql = 'insert into t_user(username,password,name) values(?,?,?)';
+    db.query(sql, [data.username, data.pwd, data.name], callback);
+};
+
+exports.getByNamePwd = function(name, pwd, callback){
+    var sql = 'select * from t_user where username = ? and password = ?';
+    db.query(sql, [name, pwd], callback);
+
+};
---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var userModel = require('../model/api');
+var app = require('../app');
+var jwt = require('jwt-simple');
+/* GET home page. */
+router.post('/login', function(req, res, next) {
+    var name = req.body.acc;
+    var pwd = req.body.pwd;
+    userModel.getByNamePwd(name, pwd, function(result){
+        var info;
+        if(result.length > 0){
+            var rs = result[0];
+            var data = {
+                username: rs.username,
+                name: rs.name,
+                id: rs.id,
+                headerPortrait: rs.headerPortrait
+            };
+            info = {
+                code: 0,
+                message: '',
+                data: data
+            }
+        } else{
+            info = {
+                code: 1,
+                message: '账号或密码错误',
+                data: {}
+            }
+        }
+        res.send(info);
+    });
+});
+router.post('/message', function (req, res, next) {
+    res.send({
+        code: 0,
+        message: '',
+        data: require('../data/message.json')
+    });
+});
+router.post('/follow', function (req, res, next) {
+    res.send({
+        code: 1,
+        message: '数据错误',
+        data: require('../data/follow.json')
+    });
+});
+module.exports = router;
